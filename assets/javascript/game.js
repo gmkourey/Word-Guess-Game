38ed1786706eb13ddeed63ef2277f6a4a20b15b0
gkourey <grant.kourey@gmail.com>
2018-07-31 20:40:08 -0700

    changed some styling and background image position

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,6 @@ var foods = ["pizza", "chicken", "sushi", "steak", "banana", "strawberry", "past
 var randomNumber = Math.floor(Math.random() * 20);
 var selection = foods[randomNumber];
 var lengthOfWord = selection.length;
-console.log(selection);
-console.log(lengthOfWord);
 var blankWord = [];
 var keyName;
 var letterIndex;
@@ -18,7 +16,6 @@ var imageArray = ["assets/images/hangman.gif", "assets/images/hangman-head.png",
 function resetGame() {
     randomNumber = Math.floor(Math.random() * 20);
     selection = foods[randomNumber];
-    console.log(selection);
     lengthOfWord = selection.length;
     missedLetters = [];
     blankWord = [];
@@ -50,35 +47,27 @@ document.addEventListener('keypress', (event) => {
     if(missedLetters.length < 6 && blankWord.length < (selection.length + 1)){
     keyName = event.key;
     keyName = keyName.toLowerCase();
-    console.log(keyName);
     letterInWord = false;
     //Condition if to make game only run if the letter is unique
     if(missedLetters.indexOf(keyName) === -1 && blankWord.indexOf(keyName) === -1) {
     for (var i = 0; i < selection.length; i++) {
         if (selection[i] === keyName) {
-            console.log('that is a letter in our word!');
             var letterIndex = selection.indexOf(keyName, i);
-            console.log(letterIndex);
             letterInWord = true;
             blankWord[letterIndex] = keyName;
         }
     }
     if (!letterInWord) {
-        console.log('This letter is not in the word');
         missedLetters.push(keyName);
         document.getElementById('picture').src = imageArray[missedLetters.length];
     }
-} else {
-    console.log('you already used that letter');
 }
     if (blankWord.join("") == selection) {
-        console.log('You Won!');
         wins++;
 
     }
 
     if (missedLetters.length == 6) {
-        console.log('You lose');
         lose++;
         document.getElementById('final-word').innerHTML = 'Your word was: ' + selection;
     }
